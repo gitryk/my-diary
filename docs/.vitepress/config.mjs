64f5577d1d0b188ad7a9c02201eb3efbdd5b0268
TryK <gitryk@users.noreply.github.com>
2025-03-21 15:48:28 +0900

    update tree

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,21 +1,33 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-  title: 'TryK\'s Note',
-  description: "내 일상과 생각을 기록하는 공간",
+  title: "TryK's Note",
+  description: 'to show content in separate pages',
   themeConfig: {
     nav: [
       { text: '홈', link: '/' },
-      { text: '소개', link: '/about' }
     ],
-    sidebar: [
-      {
-        text: '가이드',
-        items: [
-          { text: '시작하기', link: '/getting-started' },
-          { text: '사용법', link: '/usage' }
-        ]
-      }
-    ]
+    sidebar: {
+      '/openwrt/': [
+        {
+          text: 'OpenWRT',
+          collapsed: false,
+          items: [
+            { text: '기본 설정', link: '/openwrt/01-basic' },
+            { text: 'tailscale', link: '/openwrt/02-tailscale' }
+          ]
+        }
+      ],
+      '/truenas/': [
+        {
+          text: '설정',
+          collapsed: false,
+          items: [
+            { text: '기본 설정', link: '/truenas/01-basic' },
+            { text: '네트워크 설정', link: '/truenas/02-network' }
+          ]
+        }
+      ]
+    }
   }
 })
